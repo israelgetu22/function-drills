bf75d41a963f1a22b7acba690fc905ab0c2eb285
Isarel Getu <israelgetu101@gmail.com>
2022-02-14 16:14:03 -0600

    Problem Solved 5

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -73,6 +73,19 @@ greeting("Israel");
 */
 
 //CODE HERE
+const compareNums = (num1, num2) => {
+  if (num1 > num2) {
+    return num1;
+  }
+  if (num2 > num1) {
+    return num2;
+  }
+  if (num1 === num2) {
+    return num1;
+  }
+};
+compareNums(4, 14);
+console.log(compareNums(4, 14));
 
 ////////////////// PROBLEM 6 ////////////////////
 /*
